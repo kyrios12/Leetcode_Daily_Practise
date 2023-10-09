02f40520e982c9f462f17d7fb32b64e9fb35fef8
Abdul Azeez <103828615+kyrios12@users.noreply.github.com>
2023-10-09 22:26:15 +0530

    Time: 52 ms (51.43%), Space: 41.7 MB (77.2%) - LeetHub

---
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -0,0 +1,17 @@
+var filter = function(arr, fn) {
+    var filteredIndex = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            if (i !== filteredIndex) {
+                // now I will Swap current element with next available position in filtered array
+                var temp = arr[i];
+                arr[i] = arr[filteredIndex];
+                arr[filteredIndex] = temp;
+            }
+            filteredIndex++;
+        }
+    }
+    // then we can Remove the remaining elements after filteredIndex
+    arr.length = filteredIndex;
+    return arr;
+};
